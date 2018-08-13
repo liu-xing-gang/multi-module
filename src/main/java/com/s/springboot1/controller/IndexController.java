80bd996c7630e132abd78a27c6b34bff3fa5b76b
Liu Xinggang <44444536@163.com>
2018-08-13 17:28:25 +0800

    使用freemarket

---
+++ src/main/java/com/s/springboot1/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.s.springboot1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("login")
+    public String login(ModelMap mm){
+        mm.addAttribute("name", "Hello Tom");
+        return "default/login";
+    }
+}
