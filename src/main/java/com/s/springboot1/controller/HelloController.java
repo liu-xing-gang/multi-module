53e6a255c38d4cdea2ef42cfdef70392a880aacf
Liu Xinggang <44444536@163.com>
2018-08-13 18:43:29 +0800

    primary

--- src/main/java/com/s/springboot1/controller/HelloController.java
+++ src/main/java/com/s/springboot1/controller/HelloController.java
@@ -5,11 +5,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class HelloController {
     @RequestMapping("/")
     public String index(Model m){
+        List<Object> animals = new ArrayList<Object>();
+        for(int i=0; i<3; i ++){
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", "animal"+i);
+            map.put("size", "size"+i);
+            map.put("price", "price"+i);
+            animals.add(map);
+        }
+
         m.addAttribute("hello", "Hello World");
+        m.addAttribute("animals", animals);
+
         return "default/hello";
     }
 }
