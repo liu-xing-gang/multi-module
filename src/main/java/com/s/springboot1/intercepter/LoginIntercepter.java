ede2d41d3f96478e5272312395d8c195c0450377
Liu Xinggang <44444536@163.com>
2018-11-07 15:31:28 +0800

    release

--- src/main/java/com/s/springboot1/intercepter/LoginIntercepter.java
+++ src/main/java/com/s/springboot1/intercepter/LoginIntercepter.java
@@ -1,4 +1,23 @@
 package com.s.springboot1.intercepter;
 
-public class LoginIntercepter {
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginIntercepter extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        if(session.getAttribute("user") != null){
+            return true;
+        } else {
+            response.sendRedirect("/hello");
+            return false;
+        }
+    }
+
 }
--- src/main/java/com/s/springboot1/intercepter/WebConfig.java
+++ src/main/java/com/s/springboot1/intercepter/WebConfig.java
@@ -1,4 +1,17 @@
 package com.s.springboot1.intercepter;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//
+//        // 添加自定义拦截器和拦截路径
+//        registry.addInterceptor(new LoginIntercepter()).addPathPatterns("/**").excludePathPatterns("/a", "/b","/hello");
+//
+//    }
 }
